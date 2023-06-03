4910f21e947917ca2c69c7e76e3cfc577753ad6c
Daniel Santa Mateus <dsm50561@hotmail.com>
2023-06-02 19:51:59 -0500

    Homework 08

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,25 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+// Descrito en la documentación
+import SimpleLightbox from "simplelightbox";
+// Importación adicional de estilos
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+const galleryImages = galleryItems.map(({ preview, original, description }) =>
+    `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+    </li>
+    `).join("");
+
+gallery.innerHTML = galleryImages;
+gallery.style.listStyle = "none";
+
+const lightbox = new SimpleLightbox('.gallery__link', { 
+    captionsData: "alt",
+    captionDelay: 250
+});
